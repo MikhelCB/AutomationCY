dad0499d9dfe6caa3c163c52c2c01ad09f5b1289
“MikhelCB” <srrynow@hotmail.com>
2026-04-24 11:44:12 -0300

    feat: create gherkin specs and add first test case

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,46 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+class RegisterForm {
+  elements = {
+    titleInput: () => cy.get("#title"),
+    titleFeedback: () => cy.get("#titleFeedback"),
+    imageUrlInput: () => cy.get("#imageUrl"),
+    urlFeedback: () => cy.get("#urlFeedback"),
+    submitBtn: () => cy.get("#btnSubmit"),
+  };
+
+  typeTitle(text) {
+    if (!text) return;
+    this.elements.titleInput().type(text);
+  }
+
+  typeUrl(text) {
+    if (!text) return;
+    this.elements.imageUrlInput().type(text);
+  }
+}
+const registerForm = new RegisterForm();
+
+describe("Image Registration", () => {
+  describe("Submitting an image with invalid inputs", () => {
+    const input = {
+      title: "",
+      url: "",
+    };
+
+    it("Given I am on the image registration page", () => {
+      cy.visit("/");
+    });
+
+    it(`When I enter "${input.title}" in the title field`, () => {
+      registerForm.typeTitle("oieeeeee");
+    });
+    it(`Then I enter "${input.url}" in the URL field`);
+    it(`Then I click the submit button`);
+    it(
+      `Then I should see "Please type a title for the image" message above the title field`,
+    );
+    it(
+      `And I should see "Please type a valid URL" message above the imageUrl field`,
+    );
+    it(`And I should see an exclamation icon in the title and URL fields`);
+  });
+});
